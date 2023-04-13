d90f0f517d903b9045c423256d7d0e9ec58c3ef0
kevin <kevin.m.551@live.de>
2023-04-13 22:57:03 +0200

    added some colors added hero start working on somme components image search

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,6 @@
+import styles from './Navigation.module.css'
+
+export default function Navigation(){
+
+    return <div className={styles.navigation__container+' text--default'}>Navigation</div>
+}
